4129ed4df5a3138c17005a93aa063a64f8e91023
SergeShaw <sergeshaw@ip-192-168-0-101.is74.loc>
2015-02-28 18:21:11 +0500

    1.4.4

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-var btns = jQuery(".btn-choice");
+var $btns = jQuery(".btn-choice"),
+    $answer1 = jQuery("#answer1"),
+    $answer2 = jQuery("#answer2"),
+    $answer3 = jQuery("#answer3"),
+    $answer4 = jQuery("#answer4"),
+    $questionNum = jQuery("#question-num"),
+    $questionText = jQuery("#question-text");
 
 function createOnclick(value) {
 	return function() {
-			jQuery.getJSON("./ajax", function(data) {
-			jQuery("#question-text").html(data.text);
-			jQuery("#answer1").html(data.answer1);
-			jQuery("#answer2").html(data.answer2);
-			jQuery("#answer3").html(data.answer3);
-			jQuery("#answer4").html(data.answer4);
+		jQuery.getJSON("./ajax?answer=value", function(data) {
+			$questionText.html(data.text);
+			$answer1.html(data.answer1);
+			$answer2.html(data.answer2);
+			$answer3.html(data.answer3);
+			$answer4.html(data.answer4);
+			$questionNum.text(parseInt($questionNum.text())+1);
 			console.log(data);
 		});
 	};
 }
 
-for (var ii = 1; ii <= btns.length; ii++) {
-	btns[ii-1].onclick = createOnclick(ii);
+for (var ii = 1; ii <= $btns.length; ii++) {
+	$btns[ii-1].onclick = createOnclick(ii);
 }
